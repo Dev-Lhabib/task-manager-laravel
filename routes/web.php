ffc68b04dd8a74b9826a8ec0f871761b7e4039a3
Dev-Lhabib <elhabibmahfoud@gmail.com>
2026-04-27 14:42:29 -0700

    Add logout route and protect task routes with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('tasks.index');
-});
-
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 
